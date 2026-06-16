df1809676b0e1cc8f08e1086e53cd6333299b86e
edward <edwardlfish@gmail.com>
2026-06-16 14:44:12 +0300

    Replace in-memory student repository with MongoDB integration

--- src/service/studentService.js
+++ src/service/studentService.js
@@ -4,50 +4,19 @@ export const addStudent = async ({id, name, password}) => {
     return repo.addStudent({id, name, password});
 }
 
-export const findStudent = async (id) => {
-    let student = repo.findStudent(+id);
-    if (student) {
-        student = {...student};
-        student.password = undefined;
-    }
-    return student;
-}
+export const findStudent = async (id) => repo.findStudent(+id);
 
-export const deleteStudent = async (id) => {
-    let student = repo.deleteStudent(+id);
-    if (student) {
-        student.password = undefined;
-    }
-    return student;
-}
+export const deleteStudent = async (id) => repo.deleteStudent(+id);
 
-export const updateStudent = async (id, data) => {
-    let student = repo.findStudent(+id);
-    if (student) {
-        student = {...student, ...data};
-        student = {...repo.updateStudent(student)};
-        student.scores = undefined;
-    }
-    return student;
-}
+export const updateStudent = async (id, data) => repo.updateStudent(+id, data);
 
-export const addScore = async (id, exam, score) => {
-    const student = repo.findStudent(+id);
-    if (student) {
-        student.scores[exam] = score;
-        repo.updateStudent(student);
-    }
-    return student;
-}
+export const addScore = async (id, exam, score) => repo.updateStudent(+id, {[`scores.${exam}`]: score});
 
-export const findByName = async (name) => repo.findByName(name).map(s => ({...s, password: undefined}));
+export const findByName = async (name) => repo.findByName(name);
 
 export const countByNames = async (names) => {
     names = Array.isArray(names) ? names : [names];
     return repo.countByNames(names);
 }
 
-export const findByMinScore = async (exam, minScore) => repo.findByMinScore(exam, minScore).map(s => ({
-    ...s,
-    password: undefined
-}));
+export const findByMinScore = async (exam, minScore) => repo.findByMinScore(exam, +minScore);
