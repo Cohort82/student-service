c7039c4e07a750362d3e8c7ae9e3383f0d23953f
edward <edwardlfish@gmail.com>
2026-06-16 14:59:14 +0300

    Add `renameId` function to map `_id` to `id` in student service methods

--- src/service/studentService.js
+++ src/service/studentService.js
@@ -4,19 +4,27 @@ export const addStudent = async ({id, name, password}) => {
     return repo.addStudent({id, name, password});
 }
 
-export const findStudent = async (id) => repo.findStudent(+id);
+export const findStudent = async (id) => renameId(await repo.findStudent(+id));
 
-export const deleteStudent = async (id) => repo.deleteStudent(+id);
+export const deleteStudent = async (id) => renameId(await repo.deleteStudent(+id));
 
-export const updateStudent = async (id, data) => repo.updateStudent(+id, data);
+export const updateStudent = async (id, data) => renameId(await repo.updateStudent(+id, data));
 
 export const addScore = async (id, exam, score) => repo.updateStudent(+id, {[`scores.${exam}`]: score});
 
-export const findByName = async (name) => repo.findByName(name);
+export const findByName = async (name) => (await repo.findByName(name)).map(renameId);
 
 export const countByNames = async (names) => {
     names = Array.isArray(names) ? names : [names];
     return repo.countByNames(names);
 }
 
-export const findByMinScore = async (exam, minScore) => repo.findByMinScore(exam, +minScore);
+export const findByMinScore = async (exam, minScore) => (await repo.findByMinScore(exam, +minScore)).map(renameId);
+
+function renameId(student) {
+    if (student) {
+        student.id = student._id;
+        delete student._id;
+    }
+    return student;
+}
